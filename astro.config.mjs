0d129dcadbd2e16b73bde382c1945a16ad90657b
Gabriel Aedo <aedothegabriel@gmail.com>
2025-06-29 11:11:15 -0500

    Corrige base y site para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/GabrielAedoPozo',
-    base: 'Kusisa-',
+  site: 'https://gabrielaedopozo.github.io/Kusisa-/',
+  base: '/Kusisa-/',
 });
