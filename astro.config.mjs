c7a31a628da25916fdff9030c0bfc7efdabd9192
Gabriel Aedo <aedothegabriel@gmail.com>
2025-06-29 11:17:38 -0500

    Corrige site con URL exacta para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://gabrielaedopozo.github.io/Kusisa-/',
+ site: 'https://GabrielAedoPozo.github.io/Kusisa-/',
   base: '/Kusisa-/',
 });
