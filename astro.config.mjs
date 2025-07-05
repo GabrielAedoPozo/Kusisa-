babdaa49f2b6709e1379b88281a927ee703aaebf
Gabriel Aedo <aedothegabriel@gmail.com>
2025-07-05 13:53:32 -0500

    fix: config for Cloudflare + base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://GabrielAedoPozo.github.io',
-    base: '/Kusisa-/',
+  site: 'https://kusisa.pages.dev',
+  base: '/', 
 });
