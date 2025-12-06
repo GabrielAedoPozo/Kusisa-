0d67ebf97e8021f56f8f22e59825d1853ceef728
Gabriel Aedo <aedothegabriel@gmail.com>
2025-12-06 18:10:23 -0500

    important update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: 'https://kusisa.pages.dev',
   base: '/', 
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
